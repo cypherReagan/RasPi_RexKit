19d8c3650bf7159208c68813677958a11f57f4ea
cypherReagan <tbds.2001@gmail.com>
2024-04-27 15:45:52 -0500

    Added options mechanism to Menu

--- AlarmSystem/Menu.py
+++ AlarmSystem/Menu.py
@@ -10,32 +10,90 @@ Possible Options:
 3. Arm/Disarm System
 """
 
+SelectChar = 'A'
+BackChar = 'B'
+NextChar = 'C'
+PrevChar = 'D'
 
-def DisplayOptions(menuTitle, options):
+# Shows the options on the LCD and captures selections.
+# Returns index if the user selected valid option
+# Returns -1 if options are invalid
+# Returns -2 if user quits without selecting option
+#
+# Assumes the keyPad has started capturing
+def RunOptions(menuTitle, options, keyPad):
     
-    count = 1
+    retVal = -1
+    index = 0
+    done = False
+    optCount = len(options)
+    if (optCount is 0):
+        print("ERROR: RunOptions() - invalid options arg")
+    else:
     
-    for entry in options:
-        writeLCD(0, 0, menuTitle)
-        msg = "{}. ".format(count) + entry
+        increment = 0
         
-        writeLCD(0, 1, msg)
-        
-        # wait for user input
-        
-        time.sleep(2)
-        count += 1
-        LCD1602.clear()
+        while (not done):
+        #for entry in options:
+            if (index >= len(options)):
+                # reset to 1st option
+                index = 0
+                
+            if (index < 0):
+                # reset to 1st option
+                index = len(options) - 1
+                
+            entry = options[index]
+                
+            writeLCD(0, 0, menuTitle)
+            msg = "{}. ".format(index+1) + entry
+            
+            writeLCD(0, 1, msg)
+            
+            # wait for user input
+            key = keyPad.NULL
+            while (key is keyPad.NULL):
+                key = keyPad.getKey()
+                
+                if (key is SelectChar):
+                    # capture our selection
+                    retVal = index
+                    done = True
+                    break
+                elif (key is NextChar):
+                    # go to next option
+                    increment = 1
+                    break
+                elif (key is PrevChar):
+                    # go to previous option
+                    increment = -1
+                    break
+                elif (key is BackChar):
+                    # user quits
+                    retVal = -2
+                    done = True
+                    break
+                
+            index += increment
+            LCD1602.clear()
 
-    
+    return retVal
 
 # ---------
 # Test Code
 # ---------
-TEST_OPTIONS = ["Set Master PW",
-                "Setup Card",
-                "Arm System",
-                "Disarm System"]
+TestKeyPad = KP.Keypad(KP.KEYS,KP.ROW_PINS,KP.COL_PINS)
+
+TEST_OPTIONS1 = ["Set Master PW",
+                 "Setup Card",
+                 "Arm System",
+                 "Disarm System"]
+
+TEST_OPTIONS2 = ["Set PW1",
+                 "Set PW2"]
+
+MENU_NAMES = ["Options1", "Options2"]
+MENUS = [TEST_OPTIONS1, TEST_OPTIONS2]
 
 def setup():
     
@@ -59,8 +117,28 @@ if __name__ == '__main__':     # Program start from here
         time.sleep(2)
         LCD1602.clear()
         
-        DisplayOptions("Options", TEST_OPTIONS)
+        TestKeyPad.startReadKeys()
         
+        done = False
+        selectMenu = 0
+        
+        while (not done):
+            selection = RunOptions(MENU_NAMES[selectedMenu], MENUS[selectedMenu], TestKeyPad)
+            
+            if (selection >= 0):
+                print("Top Selection: ", TEST_OPTIONS1[selection])
+                if (selection is 0):
+                    selection = RunOptions("Options", TEST_OPTIONS2, TestKeyPad)
+                    
+            elif (selection is -1):
+                print("Selection ERROR!")
+                done = True
+            elif (selection is -2):
+                print("User Cancelled Top Menu")
+                done = True
+            
+        TestKeyPad.stopReadKeys()
         destroy()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, end program.
+        TestKeyPad.stopReadKeys()
         destroy()
